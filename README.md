eeac6636ec98cdf46ae000984bcdba14f8847dd1
iMohmmedSA <81385827+iMohmmedSA@users.noreply.github.com>
2026-06-24 21:12:09 +0300

    chore: init library structure with header constants

---
+++ README.md
@@ -0,0 +1,15 @@
+# jellyfin_client
+
+A Rust client library for the [Jellyfin](https://jellyfin.org) API.
+
+<!--## Usage
+
+```rust
+use jellyfin_client::Client;
+
+let client = Client::new("http://my-jellyfin:8096");
+```-->
+
+## License
+
+MIT
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub(crate) mod headers {
+    pub const AUTHORIZATION: &str = "Authorization";
+    pub const CLIENT: &str = "Client";
+    pub const DEVICE: &str = "Device";
+    pub const DEVICE_ID: &str = "DeviceId";
+    pub const VERSION: &str = "Version";
+    pub const TOKEN: &str = "Token";
+
+    pub const RETRY_AFTER: &str = "Retry-After";
+    pub const MESSAGE: &str = "Message";
+}
